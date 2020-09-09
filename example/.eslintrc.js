179f1a22a49b09090f40d5a6f3a47225a1628a98
Carolina Lopez <clopez@stackbuilders.com>
2020-09-09 17:13:30 -0500

    Improve eslint with some rules of project

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ["error", 2],
+    "max-len": ["warn", 120],
+    "no-console": "error",
+    quotes: ["error", "double"],
+    "@typescript-eslint/no-unused-vars": "off",
+  }
 };

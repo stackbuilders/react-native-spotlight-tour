ca5317fa53ca6456ea2989937879884a97f6db13
Carolina Lopez <clopez@stackbuilders.com>
2020-10-13 10:03:59 -0500

    PR Comments: Export constants directly

--- example/App.styles.ts
+++ example/App.styles.ts
@@ -1,12 +1,12 @@
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import styled from "styled-components/native";
 
-const SectionContainerView = styled.View`
+export const SectionContainerView = styled.View`
   margin-top: 32px;
   padding-horizontal: 24px;
 `;
 
-const TitleText = styled.Text`
+export const TitleText = styled.Text`
   background-color: #D3D3D3;
   font-size: 24px;
   font-weight: 600;
@@ -14,7 +14,7 @@ const TitleText = styled.Text`
   align-self: flex-start;
 `;
 
-const DescriptionText = styled.Text`
+export const DescriptionText = styled.Text`
   margin-top: 8px;
   font-size: 18px;
   font-weight: 400;
@@ -22,26 +22,17 @@ const DescriptionText = styled.Text`
   align-self: flex-start;
 `;
 
-const BoldText = styled.Text`
+export const BoldText = styled.Text`
   font-weight: 700,
 `;
 
-const SpotDescriptionView = styled.View`
+export const SpotDescriptionView = styled.View`
   background-color: #B0C4DE;
   padding: 10px;
   margin: 10px;
 `;
 
-const ButtonsGroupView = styled.View`
+export const ButtonsGroupView = styled.View`
   flex-direction: row;
   justify-content: space-between;
 `;
-
-export {
-  SectionContainerView,
-  TitleText,
-  DescriptionText,
-  BoldText,
-  SpotDescriptionView,
-  ButtonsGroupView
-};

e1ebae92a52755fe66908056b681adc5f25a87fd
Alexander Pokhil <alex.pokhil@stormotion.io>
2022-05-30 21:32:47 +0300

    feat: change styled StepView component to View with memoized style

--- src/lib/AttachStep.component.tsx
+++ src/lib/AttachStep.component.tsx
@@ -1,7 +1,5 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { StyleProp, View, ViewStyle } from "react-native";
-import styled from "styled-components/native";
-
 import { SpotlightTourContext } from "./SpotlightTour.context";
 
 interface AttachStepProps {
@@ -10,30 +8,35 @@ interface AttachStepProps {
   style?: StyleProp<ViewStyle>;
 }
 
-const StepView = styled.View`
-  align-self: flex-start;
-`;
+export const AttachStep: React.FC<AttachStepProps> = ({ children, disabled, index, style, }) => {
 
-export const AttachStep: React.FC<AttachStepProps> = ({ children, disabled, index, style }) => {
+  const [ childStyle, setChildStyle ] = useState({});
   const { current, changeSpot, spot } = useContext(SpotlightTourContext);
 
   const childRef = useRef<View>(null);
 
+  const componentStyle = useMemo<StyleProp<ViewStyle>>(
+    () => [ childStyle, style ],
+    []
+  );
+
   useEffect(() => {
     if (!spot) {
       changeSpot({ height: 0, width: 0, x: 0, y: 0 });
+      setChildStyle({ height: 0, width: 0 });
     }
 
     if (!disabled && current === index) {
       childRef.current?.measureInWindow((x, y, width, height) => {
         changeSpot({ height, width, x, y });
+        setChildStyle({ height, width });
       });
     }
   }, [current, disabled]);
 
   return (
-    <StepView ref={childRef} collapsable={false} style={style}>
+    <View ref={childRef} collapsable={false} style={componentStyle}>
       {children}
-    </StepView>
+    </View>
   );
 };

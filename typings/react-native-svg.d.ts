85547f54028ba877285aac3a2552ebfc13298a13
Jose Luis Leon <joseluis5000l@gmail.com>
2022-08-02 14:46:35 -0500

    Remove unnecessary react-native-svg types extension

--- typings/react-native-svg.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { type CommonPathProps } from "react-native-svg";
-
-declare module "react-native-svg" {
-
-  export interface MaskProps extends CommonPathProps {
-    children: React.ReactNode;
-  }
-}

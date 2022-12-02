3b46971cad7cc5e7eee927cfde7eddd12670765e
Jose Luis Leon <joseluis5000l@gmail.com>
2022-12-02 10:54:35 -0500

    Update major versions

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import { Config } from "jest";
+
+const jestConfig: Config = {
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
+  preset: "react-native",
+  setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
+  testRegex: "test/.*\\.test\\.(tsx?)$",
+  transform: {
+    "^.+\\.jsx$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/jest-runner",
+    "node_modules/(!react-native)",
+    "node_modules/(!@react-native-community)",
+  ],
+};
+
+export default jestConfig;

7ec8b02ecff4de791fceef4200dc7cae8a39cae5
Jose Luis Leon <joseluis5000l@gmail.com>
2024-07-13 22:34:04 -0500

    feat(deps): Update dependencies 2024.07.13 (#150)

--- example/metro.config.cjs
+++ example/metro.config.cjs
@@ -4,7 +4,7 @@ const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import("metro-config").MetroConfig}
  */
--- example/react-native.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  dependencies: {
-    ...(process.env.NO_FLIPPER ? { "react-native-flipper": { platforms: { ios: null } } } : {}),
-  },
-};

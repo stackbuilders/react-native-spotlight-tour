5cf2da1e12bce58feaf6f3c9ee76e333a0048207
Jose Luis Leon <joseluis5000l@gmail.com>
2025-04-07 14:58:14 -0500

    feat(core): Remove styled-components as a package dependency (#171)

--- package/src/lib/components/tour-overlay/TourOverlay.styles.ts
+++ package/src/lib/components/tour-overlay/TourOverlay.styles.ts
@@ -1,7 +1,5 @@
 import { MiddlewareData, Placement } from "@floating-ui/react-native";
-import { Platform, View } from "react-native";
-import { css } from "styled-components";
-import styled from "styled-components/native";
+import { Platform, StyleSheet, ViewStyle } from "react-native";
 
 import { Optional } from "../../../helpers/common";
 import { vh, vw } from "../../../helpers/responsive";
@@ -19,87 +17,88 @@ export const DEFAULT_ARROW: Required<ArrowOptions> = {
   size: 16,
 };
 
-export const OverlayView = styled.View`
-  height: ${vh(100)};
-  width: ${vw(100)};
-`;
+export const Css = StyleSheet.create({
+  overlayView: {
+    height: vh(100),
+    width: vw(100),
+  },
+  tooltipArrow: {
+    backgroundColor: "transparent",
+    height: 0,
+    position: "absolute",
+    width: 0,
+    zIndex: -9999,
+  },
+});
 
-export const TooltipArrow = styled(View)<TooltipArrowProps>`
-  background-color: transparent;
-  height: 0;
-  position: absolute;
-  width: 0;
-  z-index: -9999;
+export function arrowCss({ arrow, data, placement }: TooltipArrowProps): ViewStyle {
+  const { x = 0, y = 0 } = data ?? { };
+  const { color, corner, size } = typeof arrow === "number"
+    ? { ...DEFAULT_ARROW, size: arrow }
+    : { ...DEFAULT_ARROW, ...arrow };
+  // Android cannot apply specific border + radius:
+  // https://github.com/facebook/react-native/issues/9262
+  const radius = Platform.OS === "android" ? 0 : corner;
+  const h = Math.sqrt(2 * size ** 2) / 2;
+  const height = -h + 0.5;
 
-  ${({ arrow, placement, data }) => {
-    const { x = 0, y = 0 } = data ?? { };
-    const { color, corner, size } = typeof arrow === "number"
-      ? { ...DEFAULT_ARROW, size: arrow }
-      : { ...DEFAULT_ARROW, ...arrow };
-    // Android cannot apply specific border + radius:
-    // https://github.com/facebook/react-native/issues/9262
-    const radius = Platform.OS === "android" ? 0 : corner;
-    const h = Math.sqrt(2 * size ** 2) / 2;
-    const height = -h + 0.5;
-
-    switch (placement) {
-      case "bottom":
-      case "bottom-end":
-      case "bottom-start":
-        return css`
-          border-bottom-color: transparent;
-          border-bottom-width: ${size}px;
-          border-left-color: ${color.toString()};
-          border-left-width: ${size}px;
-          border-top-left-radius: ${radius}px;
-          left: ${x + h}px;
-          top: ${height}px;
-          transform: rotate(45deg);
-          transform-origin: top left;
-        `;
-      case "left":
-      case "left-end":
-      case "left-start":
-        return css`
-          border-bottom-color: transparent;
-          border-bottom-width: ${size}px;
-          border-right-color: ${color.toString()};
-          border-right-width: ${size}px;
-          border-top-right-radius: ${radius}px;
-          right: ${height}px;
-          top: ${y + h}px;
-          transform: rotate(45deg);
-          transform-origin: top right;
-        `;
-      case "right":
-      case "right-end":
-      case "right-start":
-        return css`
-          border-bottom-color: transparent;
-          border-bottom-width: ${size}px;
-          border-left-color: ${color.toString()};
-          border-left-width: ${size}px;
-          border-top-left-radius: ${radius}px;
-          left: ${height}px;
-          top: ${y + h}px;
-          transform: rotate(-45deg);
-          transform-origin: top left;
-        `;
-      case "top":
-      case "top-end":
-      case "top-start":
-        return css`
-          border-bottom-color: ${color.toString()};
-          border-bottom-left-radius: ${radius}px;
-          border-bottom-width: ${size}px;
-          border-left-color: ${color.toString()};
-          border-right-color: transparent;
-          border-right-width: ${size}px;
-          bottom: ${height}px;
-          left: ${x + h}px;
-          transform: rotate(-45deg);
-          transform-origin: bottom left;
-        `;
-    }
-  }}
-`;
+  switch (placement) {
+    case "bottom":
+    case "bottom-end":
+    case "bottom-start":
+      return {
+        borderBottomColor: "transparent",
+        borderBottomWidth: size,
+        borderLeftColor: color,
+        borderLeftWidth: size,
+        borderTopLeftRadius: radius,
+        left: x + h,
+        top: height,
+        transform: [{ rotate: "45deg" }],
+        transformOrigin: "top left",
+      };
+    case "left":
+    case "left-end":
+    case "left-start":
+      return {
+        borderBottomColor: "transparent",
+        borderBottomWidth: size,
+        borderRightColor: color,
+        borderRightWidth: size,
+        borderTopRightRadius: radius,
+        right: height,
+        top: y + h,
+        transform: [{ rotate: "45deg" }],
+        transformOrigin: "top right",
+      };
+    case "right":
+    case "right-end":
+    case "right-start":
+      return {
+        borderBottomColor: "transparent",
+        borderBottomWidth: size,
+        borderLeftColor: color,
+        borderLeftWidth: size,
+        borderTopLeftRadius: radius,
+        left: height,
+        top: y + h,
+        transform: [{ rotate: "-45deg" }],
+        transformOrigin: "top left",
+      };
+    case "top":
+    case "top-end":
+    case "top-start":
+      return {
+        borderBottomColor: color,
+        borderBottomLeftRadius: radius,
+        borderBottomWidth: size,
+        borderLeftColor: color,
+        borderRightColor: "transparent",
+        borderRightWidth: size,
+        bottom: height,
+        left: x + h,
+        transform: [{ rotate: "-45deg" }],
+        transformOrigin: "bottom left",
+      };
+  }
+}

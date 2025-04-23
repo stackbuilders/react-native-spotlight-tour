1f4d7faba5e7a59e1e82923346768e723dfafcbd
Alberto Mucarsel <albertmukarsel1397@hotmail.com>
2025-04-23 10:03:10 -0500

    Adding babel plugin

--- example/babel.config.js
+++ example/babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // added reanimated plugin
   // https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started#step-2-add-reanimateds-babel-plugin
   plugins: [
-    // '@babel/plugin-proposal-export-namespace-from', // -> reanimated for web support
+    "@babel/plugin-proposal-export-namespace-from", // -> reanimated for web support
     "react-native-paper/babel", // Bundle excluding modules you don't use: https://callstack.github.io/react-native-paper/docs/guides/getting-started
     "react-native-reanimated/plugin",
   ],

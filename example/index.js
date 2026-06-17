3594539db2bd36f44aae4219f524080fd466fc0b
Sebas Cruz <sebas.cruz750@gmail.com>
2026-06-17 09:04:36 -0500

    refactor: update example app to work with latest versions

--- example/index.js
+++ example/index.js
@@ -1,9 +1,14 @@
 import { AppRegistry, Platform } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import appInfo from "./app.json";
 import { App } from "./src/App";
 
-AppRegistry.registerComponent(appInfo.name, () => App);
+AppRegistry.registerComponent(appInfo.name, () => () => (
+  <SafeAreaProvider>
+    <App />
+  </SafeAreaProvider>
+));
 
 if (Platform.OS === "web") {
   AppRegistry.runApplication(appInfo.name, { rootTag: document.getElementById("root") });
